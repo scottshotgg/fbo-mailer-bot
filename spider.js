cea06838ed03a64f39c6eea811710f67b6c6260a
scottshotgg <scgaydos@gmail.com>
2017-03-15 13:02:24 -0500

    stupid fking excel and the SYLK ID thing - fix

--- spider.js
+++ spider.js
@@ -155,10 +155,10 @@ function createFiles(htmlHeading, tableBeginning, tableEnding) {
 
 
       //Write FBODatabase.csv file
-      var csvString = rows.map(row => Object.values(row).map(value => '"' + value + '"').join(',') + '\n');
+      var csvString = rows.map(row => '\n' + Object.values(row).map(value => '"' + value + '"').join(','));
       console.log(csvString);
 
-      fs.writeFile("FBODatabase.csv", ([':ID'].concat(attributeList).join(',')).toString() + ',\n' + csvString), function(err) {
+      fs.writeFile("FBODatabase.csv", ([':ID'].concat(attributeList).join(',')).toString() + csvString), function(err) {
         if(err) {
           return console.log(err);
         }
