908d1cb6b8352ba8dc47ae2121e2531a89e79832
scottshotgg <kind3rgartenlunchbox@gmail.com>
2017-05-10 21:08:06 -0500

    added a queue snippet

--- scraper.js
+++ scraper.js
@@ -303,6 +303,11 @@ if(process.argv[2] == 'f') {
 					mainEventLoop.emit('page', { 'pageNum': pageNum, 'form': jsForm });
 				}
 
+				var queue = [];
+				queue.push(2);         // queue is now [2]
+				queue.push(5);         // queue is now [2, 5]
+				var i = queue.shift(); // queue is now [5]
+
 			}
 
 			//console.log(body);
